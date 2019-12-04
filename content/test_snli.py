7ecf4f958748cf7a53a23090df38b5ebde7fac05
kjr21362 <kjr21362@gmail.com>
2019-12-04 13:57:36 -0800

    Added test data statistics

--- content/test_snli.py
+++ content/test_snli.py
@@ -50,6 +50,46 @@ def test(model, dataloader):
     accuracy = 0.0
     first = True
 
+    batch_i = 0
+    n_high_overlap = 0
+    n_reg_overlap = 0
+    n_low_overlap = 0
+    n_long_sentence = 0
+    n_reg_sentence = 0
+    n_short_sentence = 0
+    n_negation = 0
+    n_quantifier = 0
+    n_belief = 0
+    n_total_high_overlap = 0
+    n_total_reg_overlap = 0
+    n_total_low_overlap = 0
+    n_total_long_sentence = 0
+    n_total_reg_sentence = 0
+    n_total_short_sentence = 0
+    n_total_negation = 0
+    n_total_quantifier = 0
+    n_total_belief = 0
+    n_total_entailment = 0
+    n_total_neutral = 0
+    n_total_contradiction = 0
+    n_correct_entailment = 0
+    n_correct_neutral = 0
+    n_correct_contradiction = 0
+
+    stat_file = 'test_statistics.pkl'
+    with open(stat_file, "rb") as pkl:
+            test_statistics = pickle.load(pkl)
+
+    set_idx_high_overlap = set(test_statistics["high_overlap"])
+    set_idx_reg_overlap = set(test_statistics["reg_overlap"])
+    set_idx_low_overlap = set(test_statistics["low_overlap"])
+    set_idx_long_sentence = set(test_statistics["long_sentence"])
+    set_idx_reg_sentence = set(test_statistics["reg_sentence"])
+    set_idx_short_sentence = set(test_statistics["short_sentence"])
+    set_idx_negation = set(test_statistics["negation"])
+    set_idx_quantifier = set(test_statistics["quantifier"])
+    set_idx_belief = set(test_statistics["belief"])
+
     # Deactivate autograd for evaluation.
     with torch.no_grad():
         for batch in dataloader:
@@ -78,6 +118,104 @@ def test(model, dataloader):
                 print (labels[:5])
                 print ('0 = entailment, 1 = neutral, 2 = contradiction')
                 first = False 
+
+                # statistics
+            for i in range(len(out_classes)):
+                line_i = batch_i*32 + i
+
+                if labels[i] == 0:
+                    n_total_entailment += 1
+                elif labels[i] == 1:
+                    n_total_neutral += 1
+                elif labels[i] == 2:
+                    n_total_contradiction += 1
+
+                if line_i in set_idx_high_overlap:
+                    n_total_high_overlap += 1
+                if line_i in set_idx_reg_overlap:
+                    n_total_reg_overlap += 1
+                if line_i in set_idx_low_overlap:
+                    n_total_low_overlap += 1
+                if line_i in set_idx_long_sentence:
+                    n_total_long_sentence += 1
+                if line_i in set_idx_reg_sentence:
+                    n_total_reg_sentence += 1
+                if line_i in set_idx_short_sentence:
+                    n_total_short_sentence += 1
+                if line_i in set_idx_negation:
+                    n_total_negation += 1
+                if line_i in set_idx_quantifier:
+                    n_total_quantifier += 1
+                if line_i in set_idx_belief:
+                    n_total_belief += 1
+
+                if out_classes[i] == labels[i]:
+                    if labels[i] == 0:
+                        n_correct_entailment += 1
+                    elif labels[i] == 1:
+                        n_correct_neutral += 1
+                    elif labels[i] == 2:
+                        n_correct_contradiction += 1
+
+
+                    if line_i in set_idx_high_overlap:
+                        n_high_overlap += 1
+                    if line_i in set_idx_reg_overlap:
+                        n_reg_overlap += 1
+                    if line_i in set_idx_low_overlap:
+                        n_low_overlap += 1
+                    if line_i in set_idx_long_sentence:
+                        n_long_sentence += 1
+                    if line_i in set_idx_reg_sentence:
+                        n_reg_sentence += 1
+                    if line_i in set_idx_short_sentence:
+                        n_short_sentence += 1
+                    if line_i in set_idx_negation:
+                        n_negation += 1
+                    if line_i in set_idx_quantifier:
+                        n_quantifier += 1
+                    if line_i in set_idx_belief:
+                        n_belief += 1
+
+            batch_i += 1
+
+    print ('Total Entailment:' + str(n_total_entailment))
+    print ('Correct Entailment:' + str(n_correct_entailment))
+    print ('Accuracy:' + str(float(n_correct_entailment) / n_total_entailment))
+    print ('Total Neutral:' + str(n_total_neutral))
+    print ('Correct Neutral:' + str(n_correct_neutral))
+    print ('Accuracy:' + str(float(n_correct_neutral) / n_total_neutral))
+    
+    print ('Total Contradiction:' + str(n_total_contradiction))
+    print ('Correct Contradiction:' + str(n_correct_contradiction))
+    print ('Accuracy:' + str(float(n_correct_contradiction) / n_total_contradiction))
+    print ('Total high overlap sentence:' + str(n_total_high_overlap))
+    print ('Correct high overlap sentence:' + str(n_high_overlap))
+    print ('Accuracy:' + str(float(n_high_overlap) / n_total_high_overlap))
+    print ('Total regular overlap sentence:' + str(n_total_reg_overlap))
+    print ('Correct regular overlap sentence:' + str(n_reg_overlap))
+    print ('Accuracy:' + str(float(n_reg_overlap) / n_total_reg_overlap))
+    print ('Total low overlap sentence:' + str(n_total_low_overlap))
+    print ('Correct low overlap sentence:' + str(n_low_overlap))
+    print ('Accuracy:' + str(float(n_low_overlap) / n_total_low_overlap))
+    print ('Total long sentence:' + str(n_total_long_sentence))
+    print ('Correct long sentence:' + str(n_long_sentence))
+    print ('Accuracy:' + str(float(n_long_sentence) / n_total_long_sentence))
+    print ('Total regular sentence:' + str(n_total_reg_sentence))
+    print ('Correct regular sentence:' + str(n_reg_sentence))
+    print ('Accuracy:' + str(float(n_reg_sentence) / n_total_reg_sentence))
+    print ('Total short sentence:' + str(n_total_short_sentence))
+    print ('Correct short sentence:' + str(n_short_sentence))
+    print ('Accuracy:' + str(float(n_short_sentence) / n_total_short_sentence))
+    print ('Total sentence with negation:' + str(n_total_negation))
+    print ('Correct sentence with negation:' + str(n_negation))
+    print ('Accuracy:' + str(float(n_negation) / n_total_negation))
+    print ('Total sentence with quantifier:' + str(n_total_quantifier))
+    print ('Correct sentence with quantifier:' + str(n_quantifier))
+    print ('Accuracy:' + str(float(n_quantifier) / n_total_quantifier))
+    print ('Total sentence with belief:' + str(n_total_belief))
+    print ('Correct sentence with belief:' + str(n_belief))
+    print ('Accuracy:' + str(float(n_belief) / n_total_belief))
 
     batch_time /= len(dataloader)
     total_time = time.time() - time_start
